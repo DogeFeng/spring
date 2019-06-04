44c561f3c9993a326d3c870536061ff3d17e2382
LeeHappy <happy@git.com>
2019-06-04 10:55:05 +0800

    SpringIOC基础实现

--- spring-base/src/test/java/com/ytinf/test/TestMessage.java
+++ spring-base/src/test/java/com/ytinf/test/TestMessage.java
@@ -1,11 +1,14 @@
 package com.ytinf.test;
 
 import com.ytinf.service.IMessageService;
-import com.ytinf.service.factory.Factory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestMessage {
     public static void main(String[] args) {
-        IMessageService service = Factory.getMessageInstance("com.ytinf.service.impl.MessageServiceImpl") ;
+        // 进行配置文件的加载，利用此配置文件启动Spring容器，容器启动的时候自动进行Bean对象实例化
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring/spring-base.xml");
+        IMessageService service = context.getBean("messageService",IMessageService.class) ;
         System.out.println(service.echo("可爱的小李老师"));
     }
 }
