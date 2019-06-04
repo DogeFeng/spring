9a48a1887df8fbed9b5024668478d1f8256e87c8
LeeHappy <happy@git.com>
2019-06-04 14:54:35 +0800

    自动装配

--- spring-base/src/main/java/com/ytinf/vo/Dept.java
+++ spring-base/src/main/java/com/ytinf/vo/Dept.java
@@ -1,22 +1,11 @@
 package com.ytinf.vo;
 
-import java.beans.ConstructorProperties;
 import java.io.Serializable;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-import java.util.Set;
 
 public class Dept implements Serializable {
     private Long deptno;
     private String dname;
 
-    @ConstructorProperties(value = {"param_deptno", "param_dname"})
-    public Dept(Long deptno, String dname) {
-        this.deptno = deptno;
-        this.dname = dname;
-    }
-    // 只提供有相应的getter方法 ...
     @Override
     public String toString() {
         return "Dept{" +
@@ -29,7 +18,15 @@ public class Dept implements Serializable {
         return deptno;
     }
 
+    public void setDeptno(Long deptno) {
+        this.deptno = deptno;
+    }
+
     public String getDname() {
         return dname;
+    }
+
+    public void setDname(String dname) {
+        this.dname = dname;
     }
 }
--- spring-base/src/main/java/com/ytinf/vo/Emp.java
+++ spring-base/src/main/java/com/ytinf/vo/Emp.java
@@ -7,33 +7,14 @@ import java.util.Objects;
 public class Emp implements Serializable {
     private Long empno ;
     private String ename ;
-    private Double salary ;
-    private Date hiredate ;
-    private Integer age ;
     private Dept dept ;
 
-    public Dept getDept() {
-        return dept;
-    }
-
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Emp emp = (Emp) o;
-        return empno.equals(emp.empno) &&
-                ename.equals(emp.ename) &&
-                salary.equals(emp.salary) &&
-                age.equals(emp.age);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(empno, ename, salary, age);
-    }
-
-    public void setDept(Dept dept) {
-        this.dept = dept;
+    public String toString() {
+        return "Emp{" +
+                "empno=" + empno +
+                ", ename='" + ename + '\'' +
+                '}';
     }
 
     public Long getEmpno() {
@@ -52,38 +33,11 @@ public class Emp implements Serializable {
         this.ename = ename;
     }
 
-    public Double getSalary() {
-        return salary;
+    public Dept getDept() {
+        return dept;
     }
 
-    public void setSalary(Double salary) {
-        this.salary = salary;
-    }
-
-    public Date getHiredate() {
-        return hiredate;
-    }
-
-    public void setHiredate(Date hiredate) {
-        this.hiredate = hiredate;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "Emp{" +
-                "empno=" + empno +
-                ", ename='" + ename + '\'' +
-                ", salary=" + salary +
-                ", hiredate=" + hiredate +
-                ", age=" + age +
-                '}';
+    public void setDept(Dept dept) {
+        this.dept = dept;
     }
 }
