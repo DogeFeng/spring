550f9b535b6612527e619fdaa7d976f6280bc506
LeeHappy <happy@git.com>
2019-06-04 16:07:45 +0800

    context扫描配置

--- spring-base/src/main/java/com/ytinf/service/impl/MessageServiceImpl.java
+++ spring-base/src/main/java/com/ytinf/service/impl/MessageServiceImpl.java
@@ -1,9 +1,12 @@
 package com.ytinf.service.impl;
 
 import com.ytinf.service.IMessageService;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
+@Component  // 表示此类将自动进行Bean配置
+// 等价于：<bean id="messageServiceImpl" class="com.ytinf.service.impl.MessageServiceImpl"/>
 public class MessageServiceImpl implements IMessageService {
-
     public String echo(String msg) {
         return "【ECHO】" + msg;
     }
