d9f84b0e3f9f30a45d3753becb144d65a1867e4c
LeeHappy <happy@git.com>
2019-06-05 17:30:08 +0800

    创建消息生产者

---
+++ spring-jms-queue-producer/src/test/java/com/ytinf/test/TestJMSProducer.java
@@ -0,0 +1,25 @@
+package com.ytinf.test;
+
+import com.ytinf.service.IMessageService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@ContextConfiguration(locations = {"classpath:spring/spring-base.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+public class TestJMSProducer {
+    @Autowired
+    private IMessageService messageService;    // 注入MessageService对象
+
+    @Test
+    public void testSend() {
+        for (int x = 0; x < 100; x++) {
+            int temp = x;
+            new Thread(() -> {
+                this.messageService.send("【" + Thread.currentThread().getName() + "】晚上一起吃粽子，好嗨皮哦，消息编号：" + temp) ;
+            }, "消息生产者 - " + x).start();
+        }
+    }
+}
