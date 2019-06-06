33853dd73498501f7f03b8ace8c6445d5d8babf8
LeeHappy <happy@git.com>
2019-06-06 16:19:09 +0800

    基于Annotation实现AOP

--- spring-base/src/main/java/com/ytinf/aop/ServiceAdvice.java
+++ spring-base/src/main/java/com/ytinf/aop/ServiceAdvice.java
@@ -1,22 +1,30 @@
 package com.ytinf.aop;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
-
+@Component
+@Aspect // 表示当前是一个AOP的控制程序类
 public class ServiceAdvice {    // 该类没有任何强制性的继承要求
+    @Before(value="execution(public * com.ytinf.service..*.*(..)) and args(vo)",argNames = "vo")
     public void beforeHandle(Object param) {    // 在业务操作之前进行调用
         System.err.println("【ServiceAdvice - beforeHandle()】业务调用之前、param = " + param);
     }
+    @After("execution(public * com.ytinf.service..*.*(..))")
     public void afterHandle() {
         System.err.println("【ServiceAdvice - afterHandle()】业务调用之后。");
     }
+    @AfterThrowing(value = "execution(public * com.ytinf.service..*.*(..))",throwing = "e",argNames = "e")
     public void throwHandle(Exception exp) {  // 异常产生的后置通知
         System.err.println("【ServiceAdvice - throwHandle()】代码出现异常：" + exp);
     }
+    @AfterReturning(value="execution(public * com.ytinf.service..*.*(..))",returning = "res",argNames = "res")
     public void returnHandle(Object result) {   // 调用完毕返回结果
         System.err.println("【ServiceAdvice - returnHandle()】执行结果：" + result);
     }
+    @Around("execution(public * com.ytinf.service..*.*(..))")
     public Object aroundHandle(ProceedingJoinPoint point) { // 获取所有的请求内容
         System.err.println("【☆☆☆ ServiceAdvice - aroundHandle() - BEFORE】方法执行之前：" + Arrays.toString(point.getArgs()));
         Object result = null ; // 保存最终结果
