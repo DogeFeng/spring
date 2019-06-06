adbe359bcdee8cad7928802cafadb2ae7252a289
LeeHappy <happy@git.com>
2019-06-06 11:23:05 +0800

    Spring实现QuartZ调度

--- spring-base/src/main/java/com/ytinf/task/MyTask.java
+++ spring-base/src/main/java/com/ytinf/task/MyTask.java
@@ -1,16 +1,11 @@
 package com.ytinf.task;
 
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
-import org.springframework.scheduling.quartz.QuartzJobBean;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class MyTask extends QuartzJobBean {
+public class MyTask {
     private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS") ;
-    @Override
-    protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+    public void runJob() {
         System.out.println("【当前的日期时间】" + this.sdf.format(new Date()));
     }
 }
