56242de63884a7cee135865d98e455eba6873b8a
LeeHappy <happy@git.com>
2019-06-06 11:42:32 +0800

    基于Annotation配置SpringTask

--- spring-base/src/main/java/com/ytinf/task/MyTask.java
+++ spring-base/src/main/java/com/ytinf/task/MyTask.java
@@ -1,10 +1,14 @@
 package com.ytinf.task;
 
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
+@Component
 public class MyTask {
     private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS") ;
+    @Scheduled(cron = "* * * * * ?")
     public void runJob() {
         System.out.println("【当前的日期时间】" + this.sdf.format(new Date()));
     }
