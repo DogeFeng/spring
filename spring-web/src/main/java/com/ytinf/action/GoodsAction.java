a6237119e2eb749565763eab7136443859012c00
DogeFeng <425932842@qq.com>
2019-06-13 22:01:39 +0800

    配置完成，就差调试了

--- spring-web/src/main/java/com/ytinf/action/GoodsAction.java
+++ spring-web/src/main/java/com/ytinf/action/GoodsAction.java
@@ -1,20 +1,24 @@
 package com.ytinf.action;
 
 import com.ytinf.util.action.AbstractAction;
+import com.ytinf.vo.Goods;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/pages/admin/goods/*")
 public class GoodsAction extends AbstractAction {
     @GetMapping("goods_add")
     public String addPre(){
-        return "admin/goods/goods_add";
+        return super.getMessage("goods.add");
     }
     @PostMapping("goods_add_do")
-    public String add(){
-        return "admin/goods/goods_list";
+    public String add(Goods vo, MultipartFile photo){
+        vo.setPhotoName(super.upload(photo));
+        System.out.println(vo + "vo======");
+        return super.getMessage("goods.add");
     }
 }
